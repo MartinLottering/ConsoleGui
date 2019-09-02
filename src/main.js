0932d3bf2b59fc043061e5549174fd389763d47d
Martin Lottering <MartinLottering@capitecbank.co.za>
2019-09-02 15:25:08 +0200

    Manual build of EXE

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ const menuBuilder = require('./menuBuilder')
 let tray
 
 app.on('ready', _ => {
-  tray = new Tray(path.join('src', 'images', 'logo-16x16.png'))
+  tray = new Tray(path.join(__dirname, 'images', 'logo-16x16.png'))
   const contextMenu = menuBuilder(app, tray)
   tray.setContextMenu(contextMenu)
   tray.setToolTip('Console GUI')
